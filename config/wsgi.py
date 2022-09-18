aa51133a4afc3758c1c7d0329b67926542870c67
Amjad Masad <amjad.masad@gmail.com>
2022-09-18 22:09:40 +0900

    modify wsgi.py #1

--- config/wsgi.py
+++ config/wsgi.py
@@ -13,8 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/config")
 
-sys.path.append(os.environ["VIRTUAL_ENV"] + "/Lib/site-packages")
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
