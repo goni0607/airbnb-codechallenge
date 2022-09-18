9104d026496a86b0cf7e05e1bf02ea7d641d0b51
Amjad Masad <amjad.masad@gmail.com>
2022-09-18 19:40:40 +0900

    append path to wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/config")
+
+sys.path.append(os.environ["VIRTUAL_ENV"] + "/Lib/site-packages")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
